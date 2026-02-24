ece7d47ec31ba878ba2bdce8580fc830078346e1
tuananh0737 <rox.tuananh@gmail.com>
2026-02-24 18:34:38 +0700

    update

--- src/main/java/com/web/api/BookApi.java
+++ src/main/java/com/web/api/BookApi.java
@@ -16,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.io.IOException;
 import java.util.List;
@@ -70,8 +72,15 @@ public class BookApi {
     }
 
     @GetMapping("/public/find-all-book")
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public ResponseEntity<Page<Book>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "30") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+
+        return new ResponseEntity<>(bookPage, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/delete-book")
